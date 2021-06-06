2c061bfa6934e5f9e1816cd9374b5642ffc67cea
Ruin9999 <b14ktoss@gmail.com>
2021-06-07 01:27:07 +0800

    Added navbar transitions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
 function toggleNav() {
   var navbarcollapse = document.getElementById("navbar-collapse");
+  var navbar = document.getElementById("navbar");
+  var bodyWrapper = document.getElementById("body-wrapper");
 
   if (navbarcollapse.style.display === "none") { //Bringing the navbar back up
     navbarcollapse.style.display = "block";
+    navbar.style.width = "15%";
+    bodyWrapper.style.marginLeft = "20%";
   } else { //Hiding the navbar
     navbarcollapse.style.display = "none";
+    navbar.style.width = "5.5%";
+    bodyWrapper.style.marginLeft = "10.5%";
   }
 }
