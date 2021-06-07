af3725a2f63fb03ccf6eda7fb60310503f2082d0
Ruin9999 <b14ktoss@gmail.com>
2021-06-07 19:34:30 +0800

    Added some CSS

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,20 @@ function toggleNav() {
   var navbarcollapse = document.getElementById("navbar-collapse");
   var navbar = document.getElementById("navbar");
   var bodyWrapper = document.getElementById("body-wrapper");
+  var icon = document.getElementById("navbar-icon-image");
 
   if (navbarcollapse.style.display === "none") { //Bringing the navbar back up
     navbarcollapse.style.display = "block";
+
+    icon.style.display = "none";
+
     navbar.style.width = "15%";
-    bodyWrapper.style.marginLeft = "20%";
+    bodyWrapper.style.marginLeft = "15%";
   } else { //Hiding the navbar
     navbarcollapse.style.display = "none";
+
+    icon.style.display = "block";
+
     navbar.style.width = "5.5%";
     bodyWrapper.style.marginLeft = "10.5%";
   }
